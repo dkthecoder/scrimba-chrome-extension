295fe7b0571780e77d5a7d522ed6d5bced28bd08
Dee Kay <dk@email.com>
2023-01-19 00:15:09 +0000

    renderLeads function

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const inputEl = document.getElementById("input-el");
 let inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click", function() {
+inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    renderLeads();
 })
 
-for (let i = 0; i< myLeads.length; i++){
-    //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-    const li = document.createElement("li");
-    li.textContent = myLeads[i];
-    ulEl.append(li);
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>";
+    }
+    ulEl.innerHTML = listItems;
 }
+
